92e78b30969a39a77fb7178cb27e402f504e5d30
walker <warriorsfly@gmail.com>
2021-05-14 21:30:56 +0800

    init innovator

--- innovator/src/main.rs
+++ innovator/src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate lazy_static;
+
+mod config;
+mod constants;
+mod database;
+mod entity;
+mod handler;
+mod seravee;
+mod server;
+pub mod grpc {
+    tonic::include_proto!("grpc");
+}
+
 fn main() {
     println!("Hello, world!");
 }
