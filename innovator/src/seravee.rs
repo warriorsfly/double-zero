df9fb3de46006385a19ba554cb2e8ff339855938
walker <warriorsfly@gmail.com>
2021-05-16 23:40:30 +0800

    clean format

--- innovator/src/seravee.rs
+++ innovator/src/seravee.rs
@@ -7,7 +7,7 @@ mod tests {
 
         let mut client = MessageClient::connect("http://[::1]:50051")
             .await
-            .expect("erro   r connection");
+            .expect("error connection");
 
         let request = tonic::Request::new(Msg {
             content: "Hi, I'm allen".to_string(),
